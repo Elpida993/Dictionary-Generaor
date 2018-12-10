25d7b99e1125199475935238764fe6264e080ca9
llunknownll <45747644+llunknownll@users.noreply.github.com>
2018-12-09 23:05:31 -0500

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,37 @@
+import time
+
+length=int(raw_input("Enter the maximum of characters: "))
+name='wl.txt'    #raw_input("Enter destination file name with extension (.txt): ")
+
+tic = time.clock()
+print ("Running")
+lista=[0 for x in xrange(length)]
+x=length-1
+string="abcdefghijklnmopqrstuvwxyzABCDEFGHIJKLNMOPQRSTUVQXYZ1234567890"
+list_of_results=[]
+file1=file(name,"a")
+while(x>-1):
+    result=""
+    if lista[x]==len(string)-1:
+        for z in xrange(length):
+            result+=string[lista[z]]
+        lista[x]=0
+        x-=1
+    elif x==length-1:
+        for z in xrange(length):
+            result+=string[lista[z]]
+        lista[x]+=1
+    else:
+        for z in xrange(length):
+            result+=string[lista[z]]
+        lista[x]+=1
+        if x>0:
+            x+=1
+        else:
+            x=length-1
+    file1.write(result+"\n")
+toc = time.clock()
+ttn = toc - tic
+print ("Done! in "+str(ttn)+" seconds.");
+print ("Please check "+str(name)+" in your directory");
+
